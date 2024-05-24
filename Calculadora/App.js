2a9acbc368cd0e64c5e45d5880c3d88060e62da5
JoseMaksud <jf9718830@gmail.com>
2024-05-23 22:14:56 -0300

    Feito alguns dos botoes

--- Calculadora/App.js
+++ Calculadora/App.js
@@ -1,10 +1,28 @@
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native'
 import Botao from "./src/components/Botao";
 
 export default props => {
   return (
     <SafeAreaView style={estilos.container}>
-      <Botao />
+      <View style={estilos.botao}>
+        <Botao label="AC" triplo/>
+        <Botao label="/" />
+        <Botao label="7" />
+        <Botao label="8" />
+        <Botao label="9" />
+        <Botao label="*" />
+        <Botao label="4" />
+        <Botao label="5" />
+        <Botao label="6" />
+        <Botao label="-" />
+        <Botao label="1" />
+        <Botao label="2" />
+        <Botao label="3" />
+        <Botao label="+" />
+        <Botao label="0" duplo />
+        <Botao label="." />
+        <Botao label="=" />
+      </View>
     </SafeAreaView>
   )
 }
@@ -13,7 +31,10 @@ const estilos = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f5fcff",
-    justifyContent: "center",
-    alignItems: "center"
-  }
+    justifyContent: "flex-end",
+  },
+  botao: {
+    flexDirection: "row",
+    flexWrap: 'wrap'
+  },
 })
